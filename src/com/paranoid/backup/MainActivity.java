61afe3c0c4511d1d197c88be60f9031e51a11490
D4rKn3sSyS <jesuelfather@hotmail.com>
2012-06-03 11:11:27 -0500

    Initial commit

---
+++ src/com/paranoid/backup/MainActivity.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2011 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paranoid.backup;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.ExtendedPropertiesUtils;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends PreferenceActivity{
+    
+    private String mBackupPath;
+    private ArrayList<String> mBackupArray = new ArrayList();
+    private CharSequence[] mItems;
+    private SharedPreferences preferences;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        String mSdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mBackupPath = mSdcardPath + File.separator + "paranoid_backup" + File.separator;
+        getBackupList();
+        
+        final Preference mBackup = findPreference("backup_pad");
+        final Preference mRemove = findPreference("remove_pad");
+        final Preference mRestore = findPreference("restore_pad");
+        updateSummary(mRemove);
+        
+        mBackup.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+             public boolean onPreferenceClick(Preference preference) {
+                 Calendar mCalendar = Calendar.getInstance();
+                 String mDate = mCalendar.get(Calendar.DAY_OF_MONTH)+"."+mCalendar.get(Calendar.MONTH)+"."+
+                         mCalendar.get(Calendar.YEAR)+"."+mCalendar.get(Calendar.HOUR)+"."+mCalendar.get(Calendar.MINUTE)+"."
+                         +mCalendar.get(Calendar.SECOND);
+                 String mBackupFile = mBackupPath+mDate+".prop";
+                 RunCommands.execute(new String[]{"busybox mkdir "+mBackupPath, "busybox cp /system/pad.prop "+mBackupFile}, 0);                
+                 Toast.makeText(MainActivity.this, getString(R.string.backup_sucess).replace("%s", mBackupFile), Toast.LENGTH_LONG).show();
+                 updateSummary(mRemove);
+                 return true;
+             }
+         });
+        
+        mRestore.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+             public boolean onPreferenceClick(Preference preference) {
+                 if(getIndex() != 0){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle(R.string.restore_dialog);
+                    builder.setItems(mItems, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            RunCommands.execute(new String[]{"busybox mount -o rw,remount /system", "busybox cp "+mBackupPath+mItems[item]+" /system/pad.prop", "busybox chmod 644 /system/pad.prop", "busybox mount -o ro,remount /system"}, 0);
+                            Toast.makeText(MainActivity.this, R.string.restore_sucess, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                 } else
+                     Toast.makeText(MainActivity.this, R.string.no_backups, Toast.LENGTH_LONG).show();
+                 return true;
+             }
+         });
+        
+        mRemove.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+             public boolean onPreferenceClick(Preference preference) {
+                 if(getIndex() != 0){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle(R.string.remove_pad);
+                    builder.setItems(mItems, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            if(getIndex() == 1)
+                                RunCommands.execute(new String[]{"busybox rm -rf "+mBackupPath}, 0);
+                            else
+                                RunCommands.execute(new String[]{"busybox rm "+mBackupPath+mItems[item]}, 0);
+                            Toast.makeText(MainActivity.this, getString(R.string.remove_sucess).replace("%s", mItems[item]), Toast.LENGTH_LONG).show();
+                            updateSummary(mRemove);
+                        }
+                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                 } else
+                     Toast.makeText(MainActivity.this, R.string.no_backups, Toast.LENGTH_LONG).show();
+                 return true;
+             }
+         });
+        /*
+         * We can't detect screen automatically via API's. We need to use our ExtendedPropertiesUtils class. 
+         */
+
+        if(!preferences.getBoolean("app_started", false)){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.dagr8_prank)
+                .setCancelable(false)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        preferences.edit().putBoolean("app_started", true).commit();
+                        dialog.cancel();
+                    }
+                });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+    
+    private void updateSummary(Preference mPreference){
+        getBackupList();
+        mPreference.setSummary(getString(R.string.remove_pad_summary)+" "+getIndex());
+    }
+    
+    private int getDeviceScreen(){
+        return -1;
+    }
+    
+    private ArrayList<String> getBackupList(){
+        if(!mBackupArray.isEmpty())
+            mBackupArray.clear();
+        try{
+            File mBackupFolder = new File(mBackupPath);
+            File[] mBackups = mBackupFolder.listFiles();
+            for(int i=0; i<mBackups.length; i++){
+                mBackupArray.add(mBackups[i].getName());
+            }
+            mItems = new CharSequence[mBackupArray.size()];
+            for(int i=0; i<mItems.length; i++){
+                mItems[i] = mBackupArray.get(i);
+            }
+        } catch (Exception e){
+            // Folder is empty
+        }
+        return mBackupArray;
+    }
+    
+    private int getIndex(){
+        return mBackupArray.size();
+    }
+}
---
+++ src/com/paranoid/backup/RunCommands.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2011 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paranoid.backup;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class RunCommands implements Runnable{
+    	
+    private static String [] command = null;
+    private static int wait = 0;
+            
+    public RunCommands(String[] command, int wait) {
+        RunCommands.command=command;
+        RunCommands.wait=wait;
+    }
+    
+    public static boolean execute(String[] command, int wait) {
+        if(wait!=0){
+            try {
+                Thread.sleep(wait);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Process proc;       
+        try {        
+            proc = Runtime.getRuntime().exec("su");            
+            DataOutputStream os = new DataOutputStream(proc.getOutputStream());            
+            for (String tmpCmd : command) {
+                os.writeBytes(tmpCmd+"\n");
+            }
+            os.flush();       
+            os.close();	             
+            proc.waitFor();
+            return true;
+        } catch (IOException e) {                
+            e.printStackTrace();
+            return false;           
+        } catch (InterruptedException e) {        
+            e.printStackTrace();
+            return false;
+        } 
+    }
+
+    public void run() {
+        execute(command, wait);	
+    }
+    
+}
