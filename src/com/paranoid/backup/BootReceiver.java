6c72cd827e7a78fa069f36dae9e87c2d3dc69e0e
Paranoid4ndroid <paranoidandroidrulez@googlemail.com>
2012-06-19 11:53:38 -0500

    Update ics

--- src/com/paranoid/backup/BootReceiver.java
+++ src/com/paranoid/backup/BootReceiver.java
@@ -9,28 +9,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.StringTokenizer;
 
-public class BootReceiver extends BroadcastReceiver 
-{ 
+public class BootReceiver extends BroadcastReceiver { 
+	
     @Override 
     public void onReceive(Context context, Intent intent) { 
-    	if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
     	    final SharedPreferences sharedPreferences1 = context.getSharedPreferences(context.getPackageName() + "_preferences", Context.MODE_PRIVATE);
-         final SharedPreferences.Editor editor = sharedPreferences1.edit();
-         String Saved_Version = sharedPreferences1.getString("Version", "");
+            final SharedPreferences.Editor editor = sharedPreferences1.edit();
+            String Saved_Version = sharedPreferences1.getString("Version", "");
     	
-    	StringTokenizer tokens = new StringTokenizer(getVersion(), "-"); 
-	String first = tokens.nextToken();
-	String second = tokens.nextToken();
-	String third = tokens.nextToken();
+    	    StringTokenizer tokens = new StringTokenizer(getVersion(), "-"); 
+	    String first = tokens.nextToken();
+	    String second = tokens.nextToken();
+	    String third = tokens.nextToken();
     		
- 	f(!third.equals(Saved_Version)) {
- 	    editor.putString("Version", third);
-            editor.commit();
+ 	    if(!third.equals(Saved_Version)) {
+ 	        editor.putString("Version", third);
+                editor.commit();
  		
- 	    Intent i = new Intent(context, MainActivity.class);
- 	    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
-	    context.startActivity(i);
- 	}
+ 	        Intent i = new Intent(context, MainActivity.class);
+ 	        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+	        context.startActivity(i);
+ 	    }
 	}
   }
     
